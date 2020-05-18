234c34b22263d0607d8f520022dd2fab587a4bc3
Chaise Ballotti <42779010+ChaiseBallotti@users.noreply.github.com>
2020-05-18 10:02:52 -0400

    desktop commit
    initializing the repository.

---
+++ CompositionCalculator/src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * This project will focus on understanding the value of a given comps in team
+ * Fight Tactics, and should help improve decision making. 
+ * iteration one will focus on evaluating individual units.
+ * 
+ * Author: Chaise Ballotti
+ */
+public class Main {
+  public static void main(String[] args) {
+    // Prompt user for a input file of the current patch.
+  }
+}
---
+++ CompositionCalculator/src/Unit.java
@@ -0,0 +1,9 @@
+//Author: Chaise Ballotti
+public class Unit {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
