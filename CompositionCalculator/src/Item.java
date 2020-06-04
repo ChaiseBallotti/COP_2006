44d033cb1938c34a9d0c1f0374b0b384fd1853e9
ballo <ballo@voyager>
2020-06-04 04:44:54 -0400

    this is not used but intended to be used later

---
+++ CompositionCalculator/src/Item.java
@@ -0,0 +1,14 @@
+//author @ Chaise Ballotti
+// Items will be implemented milestone stone 3
+public class Item {
+  
+  private int health;
+  private int armor;
+  private int magicResistance;
+  private int range;
+  private int mana;
+  private int abilityPower;
+  private int damage;
+  private double atackSpeed;
+  
+}
